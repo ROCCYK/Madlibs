9e366de678a3038e62849ecbf7ee97282e83cf91
ROCCYK <82534598+ROCCYK@users.noreply.github.com>
2021-09-17 13:59:23 -0400

    1st

---
+++ main.py
@@ -0,0 +1,38 @@
+name = input('Man\'s name')
+occupation = input('occupation')
+noun = input('noun')
+noun2 = input('noun 2')
+noun3 = input('noun 3')
+noun4 = input('noun 4')
+noun5 = input('noun 5')
+noun6 = input('noun 6')
+noun7 = input('noun 7')
+noun8 = input('noun 8')
+noun9 = input('noun 9')
+shape = input('shape')
+womanname = input('Woman\'s name')
+verb = input('verb')
+verb2 = input('verb 2')
+verb3 = input('verb 3')
+verb4 = input('verb 4')
+verb5 = input('verb 5')
+verb6 = input('verb 6')
+bodypart = input('body part')
+historicmonument = input('Historic Monument')
+restaurantname = input('Restaurant Name')
+adjective = input('adjective')
+adjective2 = input('adjective 2')
+emotion = input('emotion')
+author = input('Your name')
+
+
+print(f'{name} is a normal {occupation}. Then, one day, '
+      f'a {noun} explodes, causing a {noun2} to blow up, and a nearby {noun3} erupts into a {shape} of flames. \n '
+      f'{name} realizes that he\'s being chased bt the government, who\'s trying to {verb} him. While on the run, '
+      f'he teams up with an incredibally attractive woman named {womanname}, who has an incredible\n {bodypart}. She may be from the streets,'
+      f'but she can {verb2} like nobody\'s business. The duo decide to turn the tables on their pursuers by blowing up a {noun4}, which triggers a chain\n '
+      f'reaction, causing the local {noun5}, {restaurantname}, and the {historicmonument} to explode. Then, the bad guys\' helicopter gets {verb3} by a piece of {noun5} '
+      f'from when the {noun4} exploded, and the helicopter explodes\n and falls onto a {noun6}, causing it to {verb4}, which shoots a fireball straight into the heart of '
+      f'a {noun7} and destroys the bad guy leader. Everything is {adjective} and the two decide that such a\n {adjective2} ordeal has caused them to fall in {emotion}'
+      f'with each other. They decide to celebrate by {verb5}ing on the {noun8}, and they even managed to use a {noun9} from the beggining of the movie to {verb6} the whole story together.', end='')
+print(f'\n The End written by {author}')
